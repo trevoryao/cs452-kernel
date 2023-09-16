dfe040a641d92fa57500a3393044294145ac3bdf
Trevor Yao <trevor.yao@uwaterloo.ca>
2023-09-15 21:24:34 -0400

    Add some starter code

---
+++ src/utils/rpi.h
@@ -0,0 +1,22 @@
+#ifndef _rpi_h_
+#define _rpi_h_ 1
+
+#include <stdint.h>
+#include <stddef.h>
+
+// here line denotes the serial channel
+
+#define NCH -1
+
+unsigned char uart_getc(size_t line);
+short uart_getc_nb(size_t line); // non blocking getc, return NCH if no char in UART buffer
+
+void uart_putc(size_t line, unsigned char c);
+void uart_putl(size_t line, const char *buf, size_t blen);
+void uart_puts(size_t line, const char *buf);
+void uart_printf(size_t line, char *fmt, ...);
+
+void uart_config_and_enable(size_t line, uint32_t baudrate, uint32_t stopbit);
+void uart_init();
+
+#endif /* rpi.h */
