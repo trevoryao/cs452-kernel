080e4b72e4b233e14a48121d3ba8dddfb23bbfa2
Trevor Yao <trevor.yao@uwaterloo.ca>
2023-09-19 11:11:49 -0400

    Add hardcoded kernel_state

--- kernel/include/task-state.h
+++ kernel/include/task-state.h
@@ -12,7 +12,7 @@
 enum READY_STATE {
     STATE_READY,
     STATE_BLOCKED,
-    STATE_KILLED
+    STATE_KILLED,
     N_READY_STATE
 };
 
@@ -72,7 +72,7 @@ typedef struct task_t {
     // state ends
 
     uint16_t tid;
-    task_t *parent;
+    task_t *parent; // if no parent, created by kernel
 
     enum READY_STATE ready_state;
     enum PRIORITY priority;
@@ -84,5 +84,46 @@ typedef struct task_t {
 } task_t;
 
 void task_init(task_t *t, void (*function)(), task_t *parent);
+
+typedef struct kernel_state {
+    // hardcoded assembly -- do not reorder or add before
+    // kernel state for context switching
+    int64_t x0;
+    int64_t x1;
+    int64_t x2;
+    int64_t x3;
+    int64_t x4;
+    int64_t x5;
+    int64_t x6;
+    int64_t x7;
+    int64_t x8;
+    int64_t x9;
+    int64_t x10;
+    int64_t x11;
+    int64_t x12;
+    int64_t x13;
+    int64_t x14;
+    int64_t x15;
+    int64_t x16;
+    int64_t x17;
+    int64_t x18;
+    int64_t x19;
+    int64_t x20;
+    int64_t x21;
+    int64_t x22;
+    int64_t x23;
+    int64_t x24;
+    int64_t x25;
+    int64_t x26;
+    int64_t x27;
+    int64_t x28;
+    int64_t x29;
+    int64_t x30;
+
+    uint64_t pc;
+    uint64_t sp;
+
+    int64_t pstate;
+} kernel_state;
 
 #endif
