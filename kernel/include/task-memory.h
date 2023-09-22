da3d528f53a4431dc1ada3d559914badd9bcab63
Trevor Yao <trevor.yao@uwaterloo.ca>
2023-09-22 09:25:23 -0400

    Queue management

--- kernel/include/task-memory.h
+++ kernel/include/task-memory.h
@@ -2,24 +2,37 @@
 #define __TASK_MEMORY_H__
 
 #include <task-state.h>
+#include <stddef.h>
 
 /*
  * provides functions for managing the memory of tasks
  * (slab allocation)
  */
 
-#define N_TASK_T 1024
+// 8 tasks per slab
+#define N_TASK_T 8
+#define N_SLABS 128
+
 
 typedef struct task_alloc {
     // fill me
     // array of slabs
+    
+    // TODO: Q: Is that how you wanted to use the constant? -> N of tasks would then be 8*N_TASK_T
+    task_t slabs[N_SLABS][N_TASK_T];
 
     // free list
+    task_t *free_list;
+
+    // Bitfield representing active slabs
+    uint8_t active_slab;
+
+    // array representing how many elements are in each slab
+    uint8_t task_count_in_slab[8];
 } task_alloc;
 
 void task_alloc_init(task_alloc *alloc);
-
-task_t *task_alloc_new(void);
-void task_alloc_free(task_t *task);
+task_t *task_alloc_new(task_alloc *alloc);
+void task_alloc_free(task_alloc *alloc, task_t *task);
 
 #endif
--- kernel/include/task-queue.h
+++ kernel/include/task-queue.h
@@ -4,6 +4,8 @@
 #include <task-state.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stddef.h>
+#include "util.h"
 
 /*
  * data structure for managing queued tasks.
@@ -15,19 +17,15 @@ typedef struct task_queue {
 
     task_t *front[N_PRIORITY];
     task_t *back[N_PRIORITY];
+
+    // assigning Ids missing here
+    uint16_t highest_tid;
+
 } task_queue;
 
 void task_queue_init(task_queue *tq);
-
-task_t *task_queue_pop(task_queue *tq); // pops highest priority present
-void task_queue_push(task_queue *tq, task_t *task);
-
-bool task_queue_is_empty(task_queue *tq);
-bool task_queue_is_full(task_queue *tq);
-uint32_t task_queue_size(task_queue *tq);
-
-bool task_queue_priority_is_empty(task_queue *tq, enum PRIORITY p);
-bool task_queue_priority_is_full(task_queue *tq, enum PRIORITY p);
-uint32_t task_queue_priority_size(task_queue *tq, enum PRIORITY p);
+task_t *task_queue_schedule(task_queue *tq);
+void task_queue_add(task_queue *tq, task_t *task);
+void task_queue_free_tid(task_queue *tq, uint32_t tid);
 
 #endif
