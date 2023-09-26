f359fb683cb4b967a5ca8ee3b609beb62ee66ed8
Niclas Heun <nheun@uwaterloo.ca>
2023-09-26 15:24:58 -0400

    Slab allocation

--- kernel/include/task-alloc.h
+++ kernel/include/task-alloc.h
@@ -2,6 +2,7 @@
 #define __TASK_ALLOC_H__
 
 #include <stddef.h>
+#include <string.h>
 
 // kernel
 #include "task-state.h"
@@ -22,14 +23,15 @@ typedef struct task_alloc {
     task_t *free_list;
 
     // Bitfield representing active slabs
-    uint8_t active_slab;
+    uint8_t active_slabs[N_SLABS];
 
     // array representing how many elements are in each slab
-    uint8_t task_count_in_slab[N_TASK_T];
+    uint8_t task_count_in_slab[N_SLABS];
 } task_alloc;
 
 void task_alloc_init(task_alloc *alloc);
 task_t *task_alloc_new(task_alloc *alloc);
 void task_alloc_free(task_alloc *alloc, task_t *task);
+void task_alloc_add_to_free_list(task_alloc *alloc, task_t *task);
 
 #endif
--- kernel/src/task-alloc.c
+++ kernel/src/task-alloc.c
@@ -4,28 +4,30 @@ void task_alloc_init(task_alloc *alloc) {
     // Initialize the free list to empty
     alloc->free_list = NULL;
 
-    // Init all slabs to be inactive
-    alloc->active_slab = 0;
+    // memset the slab indexes
+    memset(alloc->active_slabs, 0, sizeof(uint8_t) * N_SLABS);
 }
 
 task_t *task_alloc_new(task_alloc *alloc) {
     // no free memory in the free list
     if (!alloc->free_list) {
         // go thorough all slab and test if they could be used
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < N_SLABS; i++) {
             // consider first free slab
-            if ((alloc->active_slab & (1 << i)) == 0) {
+            if (alloc->active_slabs[i] == 0) {
                 // Initialize slab if not in use
                 for (int j = 0; j < N_TASK_T; j++) {
                     task_t *new_task = &alloc->slabs[i][j];
                     // set the slab index
                     new_task->slab_index = i;
-                    new_task->next = alloc->free_list;
-                    alloc->free_list = new_task;
+                    task_clear(new_task);
+
+                    // add it to the free list
+                    task_alloc_add_to_free_list(alloc, new_task);
                 }
 
                 // set slab in use
-                alloc->active_slab |= (1 << i);
+                alloc->active_slabs[i] = 1;
                 alloc->task_count_in_slab[i] = 0;
                 // break, because only activating one slab at a time
                 break;
@@ -50,39 +52,70 @@ task_t *task_alloc_new(task_alloc *alloc) {
     return NULL;
 }
 
-void task_alloc_free(task_alloc *alloc, task_t *task) {
-    if (task) {
-        task->ready_state = STATE_KILLED;
-
-        // Add the task to the free list
-        task->next = alloc->free_list;
+void task_alloc_add_to_free_list(task_alloc *alloc, task_t *task) {
+    // make sure the next pointer of the task is zero
+    task->next = NULL;
+    
+    // Case 1: Free list is empty
+    if (alloc->free_list == NULL) {
         alloc->free_list = task;
-
-        // decrease slab counter
-        alloc->task_count_in_slab[task->slab_index]--;
-
-        // Check if the slab can be freed
-        if (alloc->task_count_in_slab[task->slab_index] == 0) {
-            alloc->active_slab &= ~(1 << task->slab_index);
-
-            // going through the entire free list and delete all which belong to the deactivted slab
-            task_t *last_task = NULL;
-            task_t *curr_task = alloc->free_list;
-
-            while(curr_task) {
-                if (curr_task->slab_index == task->slab_index) {
-                    if (last_task) {
-                        last_task->next = curr_task->next;
-                    } else {
-                        alloc->free_list = curr_task->next;
-                    }
-                    curr_task = curr_task->next;
-                } else {
-                    last_task = curr_task;
-                    curr_task = curr_task->next;
-                }
-            }
+    } else {  
+        // Case 2: Iterate over the list and add it according to pointer
+        task_t *last = NULL;
+        task_t *current = alloc->free_list;
+        while ((current != NULL) && (current->slab_index < task->slab_index)) {
+            last = current;
+            current = current->next;
+        }
+        
+        if (current == alloc->free_list) {
+            // Case 2.1: Task is new head of free list
+            alloc->free_list = task;
+            task->next = current;
+        } else {
+            // Case 2.2: Task is added somewhere in the queue
+            last->next = task;
+            task->next = current;
         }
     }
 }
 
+void task_alloc_free(task_alloc *alloc, task_t *task) {
+    if (task != NULL) {
+        task->ready_state = STATE_KILLED;
+
+        // Add the task to the free list
+        task_alloc_add_to_free_list(alloc, task);
+
+        // decrease slab counter
+        alloc->task_count_in_slab[task->slab_index]--;
+        
+        // Check if the slab can be freed
+        if (alloc->task_count_in_slab[task->slab_index] == 0) {
+            // going through the entire free list and delete all which belong to the deactivted slab
+            uint8_t slab_index = task->slab_index;
+            task_t *prev = NULL;
+            task_t *current = alloc->free_list;
+
+            while (current != NULL) {
+                if (current->slab_index == slab_index) {
+                    if (prev == NULL) {
+                        alloc->free_list = current->next;  // if current is head of list
+                    } else {
+                        prev->next = current->next;  // unlink current from list
+                    }
+
+                    task_t *temp = current;
+                    current = current->next;
+                    temp->next = NULL;  // optional, clear next pointer of removed task
+                } else {
+                    prev = current;
+                    current = current->next;
+                }
+            }
+        
+            // set slab to inactive
+            alloc->active_slabs[task->slab_index] = 0;
+        } 
+    }
+}
