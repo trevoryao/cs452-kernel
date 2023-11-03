6a15f9f2527959dcf31c2936b114ed257d11485f
Trevor Yao <trevor.yao@uwaterloo.ca>
2023-11-03 10:27:27 -0400

    unified interface

--- track-control/include/track-control-coordinator.h
+++ track-control/include/track-control-coordinator.h
@@ -4,11 +4,16 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "controller-consts.h"
+#include "sensors.h"
+
 #define TC_MAX_DATA_LENGTH 64
 #define TC_SERVER_NAME "tc-server"
 
 enum TC_MSG_TYPE {
-    MSG_TC_PLAN_REQUEST,
+    MSG_TC_TRAIN_PUT,
+    MSG_TC_TRAIN_GET,
+    MSG_TC_SWITCH_PUT,
     MSG_TC_SENSOR_GET,
     MSG_TC_SENSOR_PUT,
     MSG_TC_MAX
@@ -18,7 +23,19 @@ typedef struct msg_tc_server {
     enum TC_MSG_TYPE type;
     uint16_t requesterTid;
     uint64_t clockTick;
-    int data[TC_MAX_DATA_LENGTH];
+
+    union data {
+        sensor sensor;      // request for sensor
+        struct {
+            uint16_t trn_no;
+            uint16_t spd;
+        } trn_cmd;
+
+        struct {
+            uint16_t sw_no;
+            enum SWITCH_DIR sw_dir;
+        } sw_cmd;
+    } data;
 } msg_tc_server;
 
 // wrappers to be send to the server
