c3d632b69931175b74a3e710304cae42442a8aa1
Trevor Yao <trevor.yao@uwaterloo.ca>
2023-11-04 17:31:53 -0400

    add sensor distance data

--- track-control/tests/routing-test/routing-test.c
+++ track-control/tests/routing-test/routing-test.c
@@ -25,9 +25,9 @@ void user_main(void) {
 
     uart_printf(CONSOLE, "Planning route...\r\n");
 
-    // A1 -> C3
+    // D4 -> B8
     uint64_t ticks = get_curr_ticks();
-    plan_route(&track[0], &track[34], 0, 77, SPD_STP, SPD_HI, &path, &speed_changes);
+    plan_route(&track[51], &track[23], -20, 77, SPD_STP, SPD_HI, &path, &speed_changes);
     ticks = get_curr_ticks() - ticks;
 
     uart_printf(CONSOLE, "Run Time (Ticks) %u\r\n", ticks);
@@ -37,12 +37,19 @@ void user_main(void) {
     uart_printf(CONSOLE, "Route:\r\n");
     while (!routing_action_queue_empty(&path)) {
         routing_action_queue_pop_front(&path, &action);
-        uassert(action.action_type == SWITCH);
-        uassert(action.delay_ticks == 0);
 
-        uart_printf(CONSOLE, "Switch %d to %c (at Sensor %c%d)\r\n",
-            action.action.sw.num, (action.action.sw.dir == STRT) ? 'S' : 'C',
-            SENSOR_MOD(action.sensor_num), SENSOR_NO(action.sensor_num));
+        if (action.action_type == SWITCH) {
+            uassert(action.info.delay_ticks == 0);
+            uart_printf(CONSOLE, "Switch %d to %c (at Sensor %c%d)\r\n",
+                action.action.sw.num, (action.action.sw.dir == STRT) ? 'S' : 'C',
+                SENSOR_MOD(action.sensor_num), SENSOR_NO(action.sensor_num));
+        } else if (action.action_type == SENSOR) {
+            uart_printf(CONSOLE, "Sensor %c%d -- dist to next: %d mm\r\n",
+                SENSOR_MOD(action.sensor_num), SENSOR_NO(action.sensor_num),
+                action.info.dist);
+        } else {
+            upanic("Incorrect Action Type: %s\r\n", action);
+        }
     }
 
     uart_printf(CONSOLE, "Speed Changes:\r\n");
@@ -52,7 +59,7 @@ void user_main(void) {
 
         char *action_str = (action.action_type == SPD_CHANGE) ? "Changed Speed to" : "Reached Speed";
         uart_printf(CONSOLE, "%s %d after %d ticks ",
-            action_str, action.action.spd, action.delay_ticks);
+            action_str, action.action.spd, action.info.delay_ticks);
 
         if (action.sensor_num != SENSOR_NONE) {
             uart_printf(CONSOLE, "(after Sensor %c%d)",
