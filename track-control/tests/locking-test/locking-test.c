b741b10ae05d4536daa796aba316ac94107fc0f3
Stefan Heun <niclas.heun@tum.de>
2023-11-19 11:30:43 -0500

    finished locking test

--- track-control/tests/locking-test/locking-test.c
+++ track-control/tests/locking-test/locking-test.c
@@ -36,9 +36,9 @@ void client1_test(void) {
     deque segments;
     deque_init(&segments, 4);
 
-    deque_push_back(&segments, 3);
-    deque_push_back(&segments, 4);
     deque_push_back(&segments, 5);
+    deque_push_back(&segments, 4);
+    deque_push_back(&segments, 3);
 
     uart_printf(CONSOLE, "Client 1 - Woke up\r\n");
 
@@ -57,7 +57,10 @@ void client1_test(void) {
     int segment = track_server_lock_one_segment_timeout(tsTid, &segments, trainNo, 100);
     uart_printf(CONSOLE, "Client 1 - locked one segments: %d\r\n", segment);
 
-
+    // check which segments are locked
+    for (int i = 0; i < 30; i++){
+        uart_printf(CONSOLE, "segment %d is locked : %d\r\n", i, track_server_segment_is_locked(tsTid, i, trainNo));
+    }
 }
 
 void client2_test(void) {
@@ -74,13 +77,19 @@ void client2_test(void) {
     deque_push_back(&segments, 4);
     deque_push_back(&segments, 6);
 
+    deque second_segments;
+    deque_init(&second_segments, 4);
+    deque_push_back(&second_segments, 10);
+    deque_push_back(&second_segments, 9);
+    deque_push_back(&second_segments, 8);
+
     Delay(clock, 2);
     uart_printf(CONSOLE, "Client 2 - Woke up\r\n");
 
     uart_printf(CONSOLE, "Client 2 -  trying to timeout lock\r\n");
     // trying to lock all with timeout
-    bool success = track_server_lock_all_segments_timeout(tsTid, &segments, trainNo, 20);
-    uart_printf(CONSOLE, "Client 2 - locked all segments timeout - should fail: %d\r\n", success);
+    int lock = track_server_lock_two_all_segments_timeout(tsTid, &segments, &second_segments, trainNo, 20);
+    uart_printf(CONSOLE, "Client 2 - locked all segments timeout - should work: %d\r\n", lock);
 
     track_server_lock_all_segments(tsTid, &segments, trainNo);
     uart_printf(CONSOLE, "Client 2 - locked all segments\r\n");
