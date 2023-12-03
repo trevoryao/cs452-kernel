530dfdbe2929938df2340dda617b4e1ce826d928
Trevor Yao <trevor.yao@uwaterloo.ca>
2023-12-03 14:56:58 -0500

    fix left and right

--- include/console-server.h
+++ include/console-server.h
@@ -6,8 +6,8 @@
 
 #define ARROW_UP (char)128
 #define ARROW_DOWN (char)129
-#define ARROW_LEFT (char)130
-#define ARROW_RIGHT (char)131
+#define ARROW_RIGHT (char)130
+#define ARROW_LEFT (char)131
 
 void console_server_main(void);
 void console_read_notifer_main(void);
